fbf2d0cd2ed816caf53694702a8f54f52ab709ca
Karen V <karenvo@usc.edu>
2019-05-17 02:38:21 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -19,7 +19,7 @@ class Scraper
   def get_page
     #responsible for using Nokogiri and open-uri to grab the entire HTML document from the web page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-    doc.css(".post").each do |post|
+      doc.css(".post").each do |post|
           course = Course.new
           course.title = post.css("h2").text
           course.schedule = post.css(".date").text
