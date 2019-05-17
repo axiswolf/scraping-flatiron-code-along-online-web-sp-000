141e612b7758cba74aa2d5b1c3048b518fe30d46
Karen V <karenvo@usc.edu>
2019-05-17 02:03:42 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -9,6 +9,7 @@ class Course
   end
   def description
     #seter and getter method for course's description
+    @description = description
   end
   def self.all
     # returns an array of all the instances of the Course's class
