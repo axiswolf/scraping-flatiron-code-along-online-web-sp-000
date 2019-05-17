8d2012e95909cc20ee3cdcf46e103eb192f07d25
Karen V <karenvo@usc.edu>
2019-05-17 02:24:53 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,7 @@ require 'open-uri'
 require_relative './course.rb'
 
 class Scraper
-  
+
     def print_courses
     self.make_courses
     Course.all.each do |course|
@@ -31,6 +31,3 @@ class Scraper
     #iterates all over the courses that gets created to puts...
   end
 end
-
-
-
