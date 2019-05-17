ab6e8b64958523b5b3f8bb93c4b62b37a93c1229
Karen V <karenvo@usc.edu>
2019-05-17 02:38:49 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -26,6 +26,7 @@ class Scraper
         course.schedule = post.css(".date").text
         course.description = post.css("p").text
       end
+    end
   # def get_courses
   #   # responsible for using a CSS selector to grab all of the HTML elements that contain a course
   # end
