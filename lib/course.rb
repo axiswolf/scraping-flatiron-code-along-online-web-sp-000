6b58c5cb80acc9152225582c86e50be2a82cfe56
Karen V <karenvo@usc.edu>
2019-05-17 02:14:36 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -2,7 +2,7 @@ class Course
   attr_accessor :title, :schedule, :description, :course
   @@all = []
   def initialize
-    @@all << self
+    @@all << course
   end
   def title(title)
     #setter and getter method for title
