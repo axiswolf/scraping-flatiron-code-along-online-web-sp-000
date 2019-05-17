e34300c2efe41dd4fb0302d3201dfa547cc00c9e
Karen V <karenvo@usc.edu>
2019-05-17 02:03:10 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,4 +1,14 @@
 class Course
-
-
+  def title
+    #setter and getter method for title
+  end
+  def schedule
+    #setter and getter method for a course's schedule
+  end
+  def description 
+    #seter and getter method for course's description
+  end
+  def self.all
+    # returns an array of all the instances of the Course's class
+  end
 end
